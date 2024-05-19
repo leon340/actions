74d1a546f3702b6d1e0cdbd4bc3ff9b594174c83
leon340 <leonbailey@outlook.com>
2024-05-19 14:40:58 -0700

    Update banking_etl.py
    fixed lynting issues

--- banking_etl.py
+++ banking_etl.py
@@ -20,51 +20,49 @@ def log_progress(message):
         f.write(timestamp + ' : ' + message + '\n')
 
 def extract():
-  extract_df = pd.DataFrame(columns=["Name", "MC_USD_Billion"])
-  soup = BeautifulSoup(requests.get(wikipedia_url).text, 'html.parser')
+    extract_df = pd.DataFrame(columns=["Name", "MC_USD_Billion"])
+    soup = BeautifulSoup(requests.get(wikipedia_url).text, 'html.parser')
+    bank_table = soup.find('table', {'class': 'wikitable'})
+    bank_table_body = bank_table.find('tbody')
+    
+    print(bank_table_body)
+    
+    for row in bank_table_body.find_all('tr')[1:]:
+        columns = row.find_all('td')
+        bank_name = columns[1].get_text(strip=True)
+        market_cap = float(columns[2].get_text(strip=True))
+        extract_df = pd.concat([extract_df,pd.DataFrame([{'Name':bank_name,'MC_USD_Billion':market_cap}])],ignore_index=True)
 
-  bank_table = soup.find('table', {'class': 'wikitable'})
-  bank_table_body = bank_table.find('tbody')
+    log_progress('Data extraction complete. Initiating Transformation process')
 
-  print(bank_table_body)
-
-  for row in bank_table_body.find_all('tr')[1:]:
-    columns = row.find_all('td')
-    bank_name = columns[1].get_text(strip=True)
-    market_cap = float(columns[2].get_text(strip=True))
-    extract_df = pd.concat([extract_df,pd.DataFrame([{'Name':bank_name,'MC_USD_Billion':market_cap}])],ignore_index=True)
-
-  log_progress('Data extraction complete. Initiating Transformation process')
-
-  return extract_df
+    return extract_df
 
 def transform(data_frame):
+    exchange_data = pd.read_csv(exchange_rate_csv)
 
-  exchange_data = pd.read_csv(exchange_rate_csv)
+    eur_rate = exchange_data.at[0,'Rate']
+    gbp_rate = exchange_data.at[1,'Rate']
+    inr_rate = exchange_data.at[2,'Rate']
 
-  eur_rate = exchange_data.at[0,'Rate']
-  gbp_rate = exchange_data.at[1,'Rate']
-  inr_rate = exchange_data.at[2,'Rate']
+    data_frame['MC_GBP_Billion'] = round((data_frame['MC_USD_Billion'] * float(gbp_rate)),2)
+    data_frame['MC_EUR_Billion'] = round((data_frame['MC_USD_Billion'] * float(eur_rate)),2)
+    data_frame['MC_INR_Billion'] = round((data_frame['MC_USD_Billion'] * float(inr_rate)),2)
 
-  data_frame['MC_GBP_Billion'] = round((data_frame['MC_USD_Billion'] * float(gbp_rate)),2)
-  data_frame['MC_EUR_Billion'] = round((data_frame['MC_USD_Billion'] * float(eur_rate)),2)
-  data_frame['MC_INR_Billion'] = round((data_frame['MC_USD_Billion'] * float(inr_rate)),2)
+    log_progress('Data transformation complete. Initiating Loading process')
 
-  log_progress('Data transformation complete. Initiating Loading process')
-
-  return data_frame
+    return data_frame
 
 def load_to_csv(bank_data,csv_load_file):
-  bank_data.to_csv(csv_load_file)
-  log_progress('Data saved to CSV file')
+    bank_data.to_csv(csv_load_file)
+    log_progress('Data saved to CSV file')
 
 def load_to_db(bank_data,conn,table_name):
-  bank_data.to_sql(table_name,conn,if_exists='replace', index=False)
+    bank_data.to_sql(table_name,conn,if_exists='replace', index=False)
 
 def run_queries(query_statement,conn):
-  query_output = pd.read_sql(query_statement, conn)
-  print(query_statement)
-  print(query_output.to_string())
+    query_output = pd.read_sql(query_statement, conn)
+    print(query_statement)
+    print(query_output.to_string())
 
 
 pd.set_option('display.max_columns', None)
